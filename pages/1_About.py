521ae8ea4c6e86acb1f3bac31bb0a6ed305eb237
Akaal62808 <khalsa88dhjd@gmail.com>
2026-03-29 17:34:29 +0530

    Add About Us page with introductory content

---
+++ pages/1_About.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title("About Us")
+
+st.write("""
+Sample Computer Centre is a leading institute
+providing quality computer education.
+
+Our mission is to develop skills for future.
+""")
